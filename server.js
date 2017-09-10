6c2dad47764b843d2af982dd11abd8e8fdcc2bc3
morgan greenwalt <you@example.com>
2017-09-10 13:45:09 -0500

    edits

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var path = require("path");
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8000;
 
 // Requiring our Note and Article models
 var Boot = require("./models/boot.js");
